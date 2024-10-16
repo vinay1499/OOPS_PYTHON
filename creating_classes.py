b10fa31a6facfc52851e5afd00ff0bc5a8482319
SAI VINAY NANDIGAM <56666558+vinay1499@users.noreply.github.com>
2024-10-16 15:28:56 -0400

    Update creating_classes

--- creating_classes.py
+++ creating_classes.py
@@ -1,43 +1,14 @@
-'''
-
-'''
-# initializing an object using __init__ method. 
-
-# class Person:
-#     pass
-
-# p1 = Person()
-# print(type(p1))
-
-# class Person:
-#     def __init__(self):         # __init__ is called dondeer method
-#         print("ran")
-
-# p1 = Person()
-# print(type(p1))
-
-# class Person:
-#     def __init__(self, x):         # __init__ is called dondeer method
-#         print(x)
-
-# p1 = Person(2)
-# print(type(p1))
-
-
-# class Person:
-#     def __init__(self, name):         # __init__ is called dondeer method
-#         self.name = name            # created an attribute.
-#                                     # you can assume self being p1, p2.....;
-
-# p1 = Person("Tim")
-# p2 = Person("Bill")
-
-# print(p1.name) # p1.name == self.name
-# print(p2.name)
-
-
+# Person class
 class Person:
-    def __init__(self, name, age):         
+    
+    '''
+    __init__ : Instantiate constructor.
+    (self, name, age): Self is referring to the instance of our Person class. Name and age are parameters.
+    self.name = name: Instance attribute
+    self.age = age : Instance attribute
+    '''
+    
+    def __init__(self, name, age): 
         self.name = name
         self.age = age            
                                    
@@ -53,9 +24,11 @@ print(p2.age)
 
 
 class Fruit:
+    
     def __init__(self, name, calories):
         self.name = name
         self.calories = calories
+        
 a = Fruit("apple", 100)
 print(a.name)
 a.color = "red"
